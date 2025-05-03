c43bfb7e76bedea24b83e8f8dc201bc93d012e8b
Jesus Santiago <jesus-contreras1@eep-igroup.com>
2025-05-03 12:25:50 +0200

    FIX new repository location  to main

--- BACK/src/test/java/com/proyectofinal/car/repository/RentalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyectofinal.car.repository;
-
-import com.proyectofinal.car.model.Rental;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RentalRepository extends JpaRepository<Rental, Long> {
-}
